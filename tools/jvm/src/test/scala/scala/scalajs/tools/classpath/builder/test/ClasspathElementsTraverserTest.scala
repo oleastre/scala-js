0acfcdac5eeb5bf3da746e6a37cb855ed598f0d9
Olivier Samyn <olivier@26lights.com>
2014-10-24 09:04:18 +0200

    Small code style corrections.

--- tools/jvm/src/test/scala/scala/scalajs/tools/classpath/builder/test/ClasspathElementsTraverserTest.scala
+++ tools/jvm/src/test/scala/scala/scalajs/tools/classpath/builder/test/ClasspathElementsTraverserTest.scala
@@ -16,26 +16,27 @@ class ClasspathElementsTraverserTest {
     protected def handleIR(relPath: String, ir: => VirtualScalaJSIRFile): Unit = ???
     protected def handleJS(js: => VirtualJSFile): Unit = ???
 
-    def test(f: File) = traverseClasspathElements(Seq(f))
+    def test(f: File): String = traverseClasspathElements(Seq(f))
   }
   
   val cpElementTraverser = new TestElementTraverser()
 
   @Test
-  def testNotExistingJarFile() {
+  def testNotExistingJarFile(): Unit = {
     val res = cpElementTraverser.test(new File("dummy.jar"))
     assertTrue(res.endsWith(cpElementTraverser.DummyVersion))
   }
 
   @Test
-  def testExistingDirectory() {
+  def testExistingDirectory(): Unit = {
     val res = cpElementTraverser.test(new File("tools/jvm/src/test/resources"))
-    assertTrue(res.indexOf(cpElementTraverser.DummyVersion)<0)
+    assertTrue(res.indexOf(cpElementTraverser.DummyVersion) < 0)
   }
 
   @Test
-  def testExistingJarFile() {
+  def testExistingJarFile(): Unit = {
     val res = cpElementTraverser.test(new File("tools/jvm/src/test/resources/test.jar"))
-    assertTrue(res.indexOf(cpElementTraverser.DummyVersion)<0)
+    assertTrue(res.indexOf(cpElementTraverser.DummyVersion) < 0)
   }
 }
+
